efeccedf071682e2be015982f3b720c01cb64169
catalinQue <75488041+catalinQue@users.noreply.github.com>
2020-12-05 10:43:27 -0500

    Add files via upload

---
+++ Day2/Inputs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day2
+{
+    internal class Inputs
+    {
+        public static string GetInputsValues()
+        {
+            var inputs = string.Empty;
+
+            using (StreamReader stream = File.OpenText("Inputs.txt"))
+            {
+                string line = string.Empty;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    inputs += line + Environment.NewLine;
+                }
+            }
+            return inputs;
+        }
+    }
+}
---
+++ Day3/PuzzleOne.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Day3
+{
+    internal class PuzzleOne
+    {
+    }
+}
